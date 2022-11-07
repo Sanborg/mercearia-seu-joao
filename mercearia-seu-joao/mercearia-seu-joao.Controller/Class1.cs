2afa77fc6bb45ccda13fad93a06a923fda31d482
Sanborg <rafasanc.rj@gmail.com>
2022-11-07 13:31:51 -0300

    Push do projeto inalterado.

---
+++ mercearia-seu-joao/mercearia-seu-joao.Controller/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace mercearia_seu_joao.Controller
+{
+    public class Class1
+    {
+    }
+}
---
+++ mercearia-seu-joao/mercearia-seu-joao.Model/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace mercearia_seu_joao.Model
+{
+    public class Class1
+    {
+    }
+}
