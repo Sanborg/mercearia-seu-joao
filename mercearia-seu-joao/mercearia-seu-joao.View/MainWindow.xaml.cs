3b7d5fb66a650583e3f3eead52a62e1d1d54c2ee
VIniWaffle <vinicius.m.araujo@estudante.firjan.senai.br>
2022-11-07 22:22:58 -0300

    Push contendo alterações na tela de login

--- mercearia-seu-joao/mercearia-seu-joao.View/MainWindow.xaml.cs
+++ mercearia-seu-joao/mercearia-seu-joao.View/MainWindow.xaml.cs
@@ -24,5 +24,43 @@ namespace mercearia_seu_joao.View
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(VerificaCampos() == true)
+            {
+                string email = boxEmail.Text;
+                string senha = boxSenha.Password;
+
+            }
+        }
+
+        private void EsqueceuSenhaUsuario(object sender, MouseButtonEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show(
+               "Contate o seu Gerente",
+               "Informação",
+               MessageBoxButton.OK,
+               MessageBoxImage.Warning
+               );
+        }
+
+        private bool VerificaCampos()
+        {
+            if(boxEmail.Text != "" && boxSenha.Password != "")
+            {
+                return true;
+            }
+            else
+            {
+              MessageBoxResult result = MessageBox.Show(
+              "Preencha todos os campos",
+              "Atenção",
+              MessageBoxButton.OK,
+              MessageBoxImage.Warning
+              );
+                return false;
+            }
+        }
     }
 }
